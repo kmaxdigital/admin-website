7f5ad3040d324c06073db60a67e068df9a81f66b
K Max Digital <admin@kmax.digital>
2023-06-16 20:16:34 +0530

    hls

--- resources/views/admin/pages/addeditmovie.blade.php
+++ resources/views/admin/pages/addeditmovie.blade.php
@@ -337,8 +337,8 @@
                                 <option value="Local" @if(isset($movie->video_type) AND $movie->video_type=="Local") selected @endif>Local</option>
                                 <option value="URL" @if(isset($movie->video_type) AND $movie->video_type=="URL") selected @endif>URL</option>
                                 <option value="Embed" @if(isset($movie->video_type) AND $movie->video_type=="Embed") selected @endif>Embed Code</option>
-                                <option value="HLS" @if(isset($movie->video_type) AND $movie->video_type=="HLS") selected @endif>HLS/m3u8</option>
-                                <option value="DASH" @if(isset($movie->video_type) AND $movie->video_type=="DASH") selected @endif>MPEG-DASH</option>                            
+<!--                                <option value="HLS" @if(isset($movie->video_type) AND $movie->video_type=="HLS") selected @endif>HLS/m3u8</option>
+    -->                            <option value="DASH" @if(isset($movie->video_type) AND $movie->video_type=="DASH") selected @endif>MPEG-DASH</option>                            
                             </select>
                       </div>
                   </div>
